682d3442bf269761923f1da904bd5aa4f55beaa6
Célestin Matte <dev@cmatte.me>
2025-09-29 16:24:14 +0200

    Add extract.sh and recomp.sh

---
+++ extract.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script extracts a libTAS movie file (.ltm)
+
+mkdir -p extract
+
+tar xzf n_recomp.ltm -C extract
---
+++ recomp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This script recreates the libTAS movie file (.ltm)
+
+tar czf n_recomp.ltm -C extract .
