74cf3e42dcb9e3460e55cdf33809c7597ac69b04
Célestin Matte <dev@cmatte.me>
2025-10-21 15:54:40 +0200

    Add most recent cheater

--- remove_cheaters.py
+++ remove_cheaters.py
@@ -8,8 +8,8 @@ connection = pymysql.connect(**DB_CONFIG)
 
 cursor = None
 
-# From https://forum.droni.es/viewtopic.php?p=176079#p176079
-CHEATERS = ['ANGERFIST', 'kryX-orange', 'L3X', 'Bonzai', 'naem', 'crappitrash', 'Goo', 'Sp33dy', 'pokemaniac1342', 'ACEOFSPADEWINS', 'Vegeta', 'BAS3', 'cheese_god', 'fuckingyourdad', 'fuckingyourmom', 'fuckingcrappitrash', 'VotedStraw61372']
+# From https://forum.droni.es/viewtopic.php?p=176079#p176079 + discussion with community
+CHEATERS = ['ANGERFIST', 'kryX-orange', 'L3X', 'Bonzai', 'naem', 'crappitrash', 'Goo', 'Sp33dy', 'pokemaniac1342', 'ACEOFSPADEWINS', 'Vegeta', 'BAS3', 'cheese_god', 'fuckingyourdad', 'fuckingyourmom', 'fuckingcrappitrash', 'VotedStraw61372', 'Donald_J_Trump']
 
 if __name__ == "__main__":
     cursor = connection.cursor(pymysql.cursors.DictCursor)
