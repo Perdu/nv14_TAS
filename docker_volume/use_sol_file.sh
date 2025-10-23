38463a5cd92e34f2b7babda14eed5138391ab415
Célestin Matte <dev@cmatte.me>
2025-10-23 23:09:13 +0200

    use_sol_file.sh: use correct sol file

--- docker_volume/use_sol_file.sh
+++ docker_volume/use_sol_file.sh
@@ -3,5 +3,5 @@
 if [ "$1" == "--no" ]; then
     rm /root/.local/share/ruffle/SharedObjects/localhost/n_v14b_userdata.sol
 else
-    cp n_v14b_userdata.sol /root/.local/share/ruffle/SharedObjects/localhost/n_v14b_userdata.sol
+   cp n_tas.sol /root/.local/share/ruffle/SharedObjects/localhost/n_v14b_userdata.sol
 fi
