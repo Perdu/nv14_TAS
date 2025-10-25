2a25647a4593219c53912a0c8fc3cc86e1d6b162
Célestin Matte <dev@cmatte.me>
2025-10-25 17:01:21 +0200

    start_level.sh: prevent overwriting existing level .ltm files

--- start_tas_level.sh
+++ start_tas_level.sh
@@ -9,6 +9,11 @@ LTM_FILE="n_base_for_levels.ltm"
 
 mkdir -p $EXTRACT_FOLDER
 
+if [[ -f "$DOCKER_VOLUME_PATH/n_levels/$1.ltm" || -f "$DOCKER_VOLUME_PATH/n_levels/$1_rta.ltm" ]]; then
+    echo "Error: One or both .ltm files already exist. Aborting."
+    exit 1
+fi
+
 tar xzf $DOCKER_VOLUME_PATH/$LTM_FILE -C $EXTRACT_FOLDER
 python start_tas_level.py $@
 tar czf $DOCKER_VOLUME_PATH/n_levels/"$1".ltm -C $EXTRACT_FOLDER .
