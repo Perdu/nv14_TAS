11f77439ced2f6bc875bf2cf22ca01ebd06789ef
Célestin Matte <dev@cmatte.me>
2025-11-13 10:30:27 +0100

    n_hitboxes.lua: use ramsearch to get x;y position automatically (unpolished)

--- volume/lua/n_hitboxes.lua
+++ volume/lua/n_hitboxes.lua
@@ -1,6 +1,6 @@
-level = "02-0"
-memy="55f7270eb778"
-local levels = dofile("lua/levels.lua")
+level = "04-3"
+memy=""
+local levels = dofile("/home/lua/levels.lua")
 
 function onPaint()
    local y_num = tonumber(memy, 16)
@@ -12,4 +12,40 @@ function onPaint()
 
    -- door
    gui.ellipse(levels[level].door_x, levels[level].door_y, 12, 12)
+end
+
+function onFrame()
+   local f = movie.currentFrame()
+   if f == 47 then
+      print(levels[level].n_y)
+      local i = ramsearch.newsearch(9, 0, 1, levels[level].n_y, "==")
+      print(string.format("nb_results newsearch: %d", i))
+   end
+   if f == 48 then
+      -- ramsearch.set_comparison_operator("")
+      local i = ramsearch.search()
+      print(string.format("nb_results: %d", i))
+      -- local v = ramsearch.get_current_value(0)
+      -- print(string.format("current value: %f", v))
+      for j = 0,i-1,1
+      do
+         local v = ramsearch.get_current_value(j)
+         -- print(string.format("current value: %f", v))
+      end
+   end
+   if f == 49 then
+      local i = ramsearch.search(0, 0, "!=")
+      print(string.format("nb_results: %d", i))
+      if i == 1 then
+         memy = ramsearch.get_address(j)
+      else
+         print("Error: found too many values")
+         for j = 0,i-1,1
+         do
+            local v = ramsearch.get_current_value(j)
+            local addr = ramsearch.get_address(j)
+            print(string.format("current value: %f @ %s", v, addr))
+         end
+      end
+   end
 end
