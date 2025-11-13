df9bf6b6bad8db1600eeb703cdb5a2bd237b17ae
Célestin Matte <dev@cmatte.me>
2025-11-13 20:13:06 +0100

    Update n_start.lua to get x;y pos information

--- volume/lua/n_start.lua
+++ volume/lua/n_start.lua
@@ -12,11 +12,34 @@ local done = false
 local triggered = false
 local need_unpause = false
 
+local level = "04-3"
+local memy=""
+local levels = dofile("/home/lua/levels.lua")
+local dbg = true
+
+local space_frame = -100
+local pos_found = false
+
+function onPaint()
+   if memy ~= "" then
+      local y_num = tonumber(memy, 16)
+      local y = memory.readd(y_num)
+      local x_num = y_num - 56
+      local x = memory.readd(x_num)
+      gui.ellipse(x, y, 10, 10)
+      gui.text(150, 580, string.format("%f ; %f", x, y))
+   end
+
+   -- door
+   gui.ellipse(levels[level].door_x, levels[level].door_y, 12, 12)
+end
+
 -- This runs each time the game (process) starts.
 function onStartup()
     -- Reset session variables (important when restarting the game)
     done = false
     triggered = false
+    pos_found = false
 
     -- Request an unpause on first frame if needed
     need_unpause = ASSUME_STARTS_PAUSED
@@ -33,10 +56,6 @@ end
 
 -- Perform runtime actions (must be done in onFrame)
 function onFrame()
-    if done then return end
-
-    -- check_space()
-
     -- Unpause at startup if requested
     if need_unpause then
         runtime.playPause()
@@ -45,13 +64,56 @@ function onFrame()
 
     -- If Space was pressed, save and pause
     if triggered then
+       space_frame = movie.currentFrame()
         runtime.saveState(SAVE_SLOT)
-        runtime.playPause()
+        if dbg then
+           print(string.format("nb_results newsearch: %f", levels[level].n_y))
+        end
+        local i = ramsearch.newsearch(9, 0, 1, levels[level].n_y, "==")
+        if dbg then
+           print(string.format("nb_results newsearch: %d", i))
+        end
 
         -- Disable for this session only
         done = true
         triggered = false
     end
+
+   if not pos_found then
+      local f = movie.currentFrame()
+      if f == space_frame + 1 then
+         local i = ramsearch.search()
+         if dbg then
+            print(string.format("nb_results: %d", i))
+            for j = 0,i-1,1
+            do
+               local v = ramsearch.get_current_value(j)
+               print(string.format("current value: %f", v))
+            end
+         end
+      elseif f == space_frame + 2 then
+         local i = ramsearch.search(0, 0, "!=")
+         if dbg then
+            print(string.format("nb_results: %d", i))
+         end
+         if i == 1 then
+            memy = ramsearch.get_address(j)
+         else
+            print("Error: found too many values")
+            if dbg then
+               for j = 0,i-1,1
+               do
+                  local v = ramsearch.get_current_value(j)
+                  local addr = ramsearch.get_address(j)
+                  print(string.format("current value: %f @ %s", v, addr))
+               end
+            end
+         end
+         pos_found = true
+         runtime.loadState(SAVE_SLOT)
+         runtime.playPause()
+      end
+   end
 end
 
 -- Register callbacks
