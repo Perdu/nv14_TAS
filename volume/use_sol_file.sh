1502477b45999ba894273445d7580484a93b0d39
Célestin Matte <dev@cmatte.me>
2025-12-08 16:41:28 +0100

    use_sol_file: add --override option to copy generic sol file and get new score even if no gain + sol file

--- volume/use_sol_file.sh
+++ volume/use_sol_file.sh
@@ -2,6 +2,8 @@
 
 if [ "$1" == "--no" ]; then
     rm /root/.local/share/ruffle/SharedObjects/localhost/n_v14b_userdata.sol
+elif [ "$1" == "--override" ]; then
+   cp n_tas_lowscores.sol /root/.local/share/ruffle/SharedObjects/localhost/n_v14b_userdata.sol
 else
    cp n_tas.sol /root/.local/share/ruffle/SharedObjects/localhost/n_v14b_userdata.sol
 fi
