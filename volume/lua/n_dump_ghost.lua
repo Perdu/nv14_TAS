93d696024ef920ce152f284653ad1487756c5e6a
Célestin Matte <dev@cmatte.me>
2025-12-11 16:22:49 +0100

    dump_ghost: also dump speed

--- volume/lua/n_dump_ghost.lua
+++ volume/lua/n_dump_ghost.lua
@@ -17,6 +17,7 @@ local need_unpause = false
 
 local level = nil
 local memy=""
+local memspeed_y=""
 local levels = dofile("/home/lua/levels.lua")
 local dbg = true
 local ghostFilePath = nil
@@ -24,6 +25,7 @@ local ghostFile = nil
 
 local space_frame = -100
 local pos_found = false
+local ramsearch_done = false
 local pre_reload_skipped = false
 
 function onPaint()
@@ -46,6 +48,8 @@ function onStartup()
     -- Request an unpause on first frame if needed
     need_unpause = ASSUME_STARTS_PAUSED
 
+    memy = ""
+    memspeed_y = ""
     level = movie.getMovieFileName():match("/(%d+-%d+).*%.ltm$")
     ghostFilePath = "/home/ghosts/" .. level .. ".csv"
     ghostFile = io.open(ghostFilePath, "w")
@@ -87,51 +91,80 @@ function onFrame()
         triggered = false
     end
 
-   if not pos_found then
-      if f == space_frame + 1 then
-         local i = ramsearch.search()
-         if dbg then
-            print(string.format("nb_results: %d", i))
-            for j = 0,i-1,1
-            do
-               local v = ramsearch.get_current_value(j)
-               print(string.format("current value: %f", v))
-            end
-         end
-      elseif f == space_frame + 2 then
-         local i = ramsearch.search(0, 0, "!=")
-         if dbg then
-            print(string.format("nb_results: %d", i))
-         end
-         if i == 1 then
-            memy = ramsearch.get_address(j)
-            pos_found = true
-         else
-            print(string.format("Error: found too many values (%d)", i))
-            for j = 0,i-1,1
-            do
-               local v = ramsearch.get_current_value(j)
-               local addr = ramsearch.get_address(j)
-               local addr_x = tonumber(addr, 16) - 56
-               local x = memory.readd(addr_x)
-               if dbg then
-                  print(string.format("Value %d: %f @%s", j, v, addr))
-                  print(string.format("Corresponding x value @%s : %f", addr_x, x))
-               end
-               if x == levels[level].n_y then
-                  -- we finally found the right one
-                  memy = ramsearch.get_address(j)
-                  print("Found!")
-                  pos_found = true
-                  break
-               end
-            end
-         end
-         runtime.loadState(SAVE_SLOT)
-      end
-   end
+    if not ramsearch_done then
+       local f = movie.currentFrame()
+       if not pos_found then
+          if f == space_frame + 1 then
+             local i = ramsearch.search()
+             if dbg then
+                print(string.format("nb_results: %d", i))
+                for j = 0,i-1,1
+                do
+                   local v = ramsearch.get_current_value(j)
+                   print(string.format("current value: %f", v))
+                end
+             end
+          elseif f == space_frame + 2 then
+             local i = ramsearch.search(0, 0, "!=")
+             if dbg then
+                print(string.format("nb_results: %d", i))
+             end
+             if i == 1 then
+                memy = ramsearch.get_address(j)
+                pos_found = true
+             else
+                print(string.format("Error: found too many values (%d)", i))
+                for j = 0,i-1,1
+                do
+                   local v = ramsearch.get_current_value(j)
+                   local addr = ramsearch.get_address(j)
+                   local addr_x = tonumber(addr, 16) - 56
+                   local x = memory.readd(addr_x)
+                   if dbg then
+                      print(string.format("Value %d: %f @%s", j, v, addr))
+                      print(string.format("Corresponding x value @%s : %f", addr_x, x))
+                   end
+                   if x == levels[level].n_y then
+                      -- we finally found the right one
+                      memy = ramsearch.get_address(j)
+                      print("Found!")
+                      pos_found = true
+                      break
+                   end
+                end
+             end
+          end
+       end
+       -- And now, speed
+       if f == space_frame + 2 then
+          i = ramsearch.newsearch(9, 0, 1, 0.14, ">")
+          if dbg then
+             print(string.format("nb_results newsearch speed: %d", i))
+          end
+          i = ramsearch.search(1, 0.16, "<")
+          if dbg then
+             print(string.format("nb_results search speed 2: %d", i))
+          end
+       elseif f == space_frame + 3 then
+          local i = ramsearch.search(1, 0.29, ">")
+          if dbg then
+             print(string.format("nb_results search speed 3: %d", i))
+          end
+          i = ramsearch.search(1, 0.30, "<")
+          if dbg then
+             print(string.format("nb_results search speed 4: %d", i))
+          end
+          if i == 1 then
+             memspeed_y = ramsearch.get_address(0)
+             print(string.format("memspeed_y: %s", memspeed_y))
+          end
+          ramsearch_done = true
+          runtime.loadState(SAVE_SLOT)
+          advance_one_step_after_ramsearch = 0
+       end
+    end
 
-   if pos_found then
+   if ramsearch_done then
       if not pre_reload_skipped then
          -- since onPaint() is called on the start + 2 frame before
          -- the reload, we need to skip this frame to avoid
@@ -147,9 +180,14 @@ function onFrame()
             local shift = (input.getKey(KEY_SHIFT) ~= 0) and 1 or 0
             local left  = (input.getKey(KEY_LEFT)  ~= 0) and 1 or 0
             local right = (input.getKey(KEY_RIGHT) ~= 0) and 1 or 0
+            -- speed
+            local y_num_speed = tonumber(memspeed_y, 16)
+            local vy = memory.readd(y_num_speed)
+            local x_num_speed = y_num_speed - 56
+            local vx = memory.readd(x_num_speed)
 
-            print(string.format("%d,%f,%f,%d,%d,%d", f, x, y, shift, left, right))
-            ghostFile:write(string.format("%d,%f,%f,%d,%d,%d\n", f - space_frame, x, y, shift, left, right))
+            print(string.format("%d,%f,%f,%d,%d,%d,%f,%f", f, x, y, shift, left, right, vx, vy))
+            ghostFile:write(string.format("%d,%f,%f,%d,%d,%d,%f,%f\n", f - space_frame, x, y, shift, left, right, vx, vy))
          end
       end
    end
