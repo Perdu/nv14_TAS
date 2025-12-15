26b07a8900390d57113a38e6d506fbc35d593d36
Célestin Matte <dev@cmatte.me>
2025-12-15 22:00:16 +0100

    ramseearch: fix first frames and pause for dump_ghost

--- volume/lua/n_dump_ghost.lua
+++ volume/lua/n_dump_ghost.lua
@@ -67,7 +67,23 @@ function onInput()
        end
     end
 
-   dofile("/home/lua/lib/n_position_ramsearch_oninput.lua")
+    if not ramsearch_done and grounded_levels[level] and not original_input_modified then
+       local f = movie.currentFrame()
+       if f == space_frame + 1 then
+          shift_pressed = input.getKey(KEY_SHIFT)
+          print(string.format("Shift pressed? %d", shift_pressed))
+          input.setKey(KEY_SHIFT, 1)
+          original_input_modified = true
+       end
+    elseif ramsearch_done and grounded_levels[level] and original_input_modified then
+       if movie.currentFrame() == space_frame + 1 then
+          -- put original value back
+          print("Putting back shift value:", shift_pressed)
+          input.setKey(KEY_SHIFT, shift_pressed)
+          original_input_modified = false
+       end
+    end
+
 end
 
 -- Perform runtime actions (must be done in onFrame)
