2ea1ab06db5c70ff64518c22a978fc34ff4af66b
Célestin Matte <dev@cmatte.me>
2026-01-05 22:22:26 +0100

    Add ghost files for grounded levels

--- volume/lua/lib/grounded_levels.lua
+++ volume/lua/lib/grounded_levels.lua
@@ -1,5 +1,11 @@
 grounded_levels = {
    ["01-0"] = true,
    ["02-0"] = true,
-   ["03-1"] = true
+   ["03-1"] = true,
+   ["37-0"] = true,
+   ["63-1"] = true,
+   ["80-2"] = true,
+   ["81-0"] = true,
+   ["82-0"] = true,
+   ["87-0"] = true
 }
