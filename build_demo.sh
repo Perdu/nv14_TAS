1229a0c208d4164761462f2df4c8c9fed9558d11
Célestin Matte <dev@cmatte.me>
2026-01-17 13:08:17 +0100

    build_demo.sh: add --record option to launch docker for recording

--- build_demo.sh
+++ build_demo.sh
@@ -10,8 +10,26 @@ DOCKER_VOLUME_PATH="volume"
 BASE_LTM_FILE="n_base_for_levels.ltm"
 LTM_FILE="n_speedrun.ltm"
 
+RECORD=false
+PASSTHROUGH_ARGS=()
+# extract --record or -r from arguments
+for arg in "$@"; do
+    case "$arg" in
+        --record|-r)
+            RECORD=true
+            ;;
+        *)
+            PASSTHROUGH_ARGS+=("$arg")
+            ;;
+    esac
+done
+
 mkdir -p $EXTRACT_FOLDER
 
 tar xzf $BASE_LTM_FILE -C $EXTRACT_FOLDER
-python builder.py $@
+python builder.py "${PASSTHROUGH_ARGS[@]}"
 tar czf $DOCKER_VOLUME_PATH/$LTM_FILE -C $EXTRACT_FOLDER .
+
+if [ "$RECORD" = true ]; then
+    docker run --rm -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/root/.Xauthority:rw --net=host -v $SCRIPT_DIR/volume:/home/ libtas_recording
+fi
