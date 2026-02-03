fa89fe74a9de9318321ed971f0089747ee666d61
Célestin Matte <dev@cmatte.me>
2026-02-03 20:11:16 +0100

    build_demo.sh: add --youtube option

--- build_demo.sh
+++ build_demo.sh
@@ -10,13 +10,17 @@ DOCKER_VOLUME_PATH="volume"
 BASE_LTM_FILE="n_base_for_levels.ltm"
 LTM_FILE="n_speedrun.ltm"
 
-RECORD=false
+RECORD=0
+YOUTUBE=0
 PASSTHROUGH_ARGS=()
 # extract --record or -r from arguments
 for arg in "$@"; do
     case "$arg" in
         --record|-r)
-            RECORD=true
+            RECORD=1
+            ;;
+        --youtube|-y)
+            YOUTUBE=1
             ;;
         *)
             PASSTHROUGH_ARGS+=("$arg")
@@ -30,6 +34,11 @@ tar xzf $BASE_LTM_FILE -C $EXTRACT_FOLDER
 python3 builder.py "${PASSTHROUGH_ARGS[@]}"
 tar czf $DOCKER_VOLUME_PATH/$LTM_FILE -C $EXTRACT_FOLDER .
 
-if [ "$RECORD" = true ]; then
-    docker run --rm -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/root/.Xauthority:rw --net=host -v $SCRIPT_DIR/volume:/home/ libtas_recording
+if [ "$RECORD" -eq 1 ]; then
+    if [ "$YOUTUBE" -eq 1 ]; then
+        IMAGE="libtas_recording_youtube"
+    else
+        IMAGE="libtas_recording"
+    fi
+    docker run --rm -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/root/.Xauthority:rw --net=host -v $SCRIPT_DIR/volume:/home/ $IMAGE
 fi
