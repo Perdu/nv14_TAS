0809998616253aa123fce5ab1ef6fe4fa8773648
Célestin Matte <dev@cmatte.me>
2026-02-03 20:41:09 +0100

    encode.sh: add option to open mpv immediately

--- encode.sh
+++ encode.sh
@@ -9,12 +9,16 @@ DOCKER_VOLUME_PATH="volume"
 
 YOUTUBE=0
 GHOST=0
+OPEN_MPV=0
 PASSTHROUGH_ARGS=()
 # extract --record or -r from arguments
 for arg in "$@"; do
     case "$arg" in
         --youtube|-y)
             YOUTUBE=1
+            ;;
+        --mpv|--open-mpv|-m)
+            OPEN_MPV=1
             ;;
         --ghost|-g)
             GHOST=1
@@ -26,7 +30,7 @@ for arg in "$@"; do
 done
 
 if [ "$LEVEL" == "" ]; then
-    echo "Usage: $0 [--youtube|-y] [--ghost|-g] LEVEL"
+    echo "Usage: $0 [--youtube|-y] [--ghost|-g] [--mpv|--open-mpv|-m] LEVEL"
     exit 1
 fi
 
@@ -36,3 +40,7 @@ else
     IMAGE="libtas_recording"
 fi
 docker run --rm -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/root/.Xauthority:rw --net=host -v $SCRIPT_DIR/volume:/home/ $IMAGE bash -c "/root/src/libTAS/build/AppDir/usr/bin/libTAS -n -r /home/n_levels/$LEVEL.ltm --lua /home/lua/n_ghost.lua -d /home/n_demos/$LEVEL.mp4 /root/src/ruffle/target/release/ruffle_desktop -g gl --no-gui --width 792 /home/n_v14.swf ; while ffprobe /home/n_demos/$LEVEL.mp4 2>&1 | grep -q 'moov atom not found'; do sleep 1 ; done "
+
+if [ $OPEN_MPV -eq 1 ]; then
+    mpv volume/n_demos/$LEVEL.mp4
+fi
