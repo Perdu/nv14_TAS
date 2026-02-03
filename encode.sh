a5702c6fb3941bfee194a4c4d048332533e4a32e
Célestin Matte <dev@cmatte.me>
2026-02-03 20:29:21 +0100

    Add encode.sh, to encode automatically

---
+++ encode.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# This script encodes a given level fully automatically (unlike build_demo.sh)
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "$SCRIPT_DIR"
+
+DOCKER_VOLUME_PATH="volume"
+
+YOUTUBE=0
+GHOST=0
+PASSTHROUGH_ARGS=()
+# extract --record or -r from arguments
+for arg in "$@"; do
+    case "$arg" in
+        --youtube|-y)
+            YOUTUBE=1
+            ;;
+        --ghost|-g)
+            GHOST=1
+            ;;
+        *)
+            LEVEL="$arg"
+            ;;
+    esac
+done
+
+if [ "$LEVEL" == "" ]; then
+    echo "Usage: $0 [--youtube|-y] [--ghost|-g] LEVEL"
+    exit 1
+fi
+
+if [ "$YOUTUBE" -eq 1 ]; then
+    IMAGE="libtas_recording_youtube"
+else
+    IMAGE="libtas_recording"
+fi
+docker run --rm -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/root/.Xauthority:rw --net=host -v $SCRIPT_DIR/volume:/home/ $IMAGE /root/src/libTAS/build/AppDir/usr/bin/libTAS -n -r /home/n_levels/$LEVEL.ltm --lua /home/lua/n_ghost.lua -d /home/n_demos/$LEVEL.mp4 /root/src/ruffle/target/release/ruffle_desktop -g gl --no-gui --width 792 /home/n_v14.swf
