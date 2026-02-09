e0a6e9c1f2718034073565b06342fde7b71042f1
Célestin Matte <dev@cmatte.me>
2026-02-09 11:51:57 +0100

    Also fix hash for n_base_for_levels.ltm

--- fix_game_hash.sh
+++ fix_game_hash.sh
@@ -7,7 +7,7 @@ cd "$SCRIPT_DIR"
 
 HASH="fcc2c6d4912aa52b8c4f68a31a6eab74"
 
-for i in volume/n_levels/[0-9][0-9]-[0-9].ltm ; do
+for i in volume/n_levels/[0-9][0-9]-[0-9].ltm n_base_for_levels.ltm ; do
     echo "$i"
     if $(tar xOzf "$i" config.ini | grep -vq md5=$HASH); then
         echo "Fixing $i"
