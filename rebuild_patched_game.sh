ea0982b40923cad4cc8223f73fe4f25b944102b6
Célestin Matte <dev@cmatte.me>
2026-03-29 23:41:21 +0200

    rebuild_patched_game: install ffdec if missing

--- rebuild_patched_game.sh
+++ rebuild_patched_game.sh
@@ -3,4 +3,11 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$SCRIPT_DIR"
 
+which ffdec >/dev/null
+if [ $? -ne 0 ]; then
+    echo "ffdec (JPEXS) missing, installing it..."
+    wget https://github.com/jindrapetrik/jpexs-decompiler/releases/download/version25.1.3/ffdec_25.1.3.deb
+    sudo apt-get install ./ffdec_25.1.3.deb
+fi
+
 ffdec -importScript volume/n_v14.swf volume/n_v14_patched.swf decompiled_source/
