b2bc89cf9ea0fb19cabb7dd61a136be48ba950f2
Célestin Matte <dev@cmatte.me>
2026-09-05 17:16:48 +0200

    change_author.sh: add option to change author of hs files

--- change_author.sh
+++ change_author.sh
@@ -7,12 +7,17 @@ EXTRACT_FOLDER="extract"
 DOCKER_VOLUME_PATH="volume"
 
 if [ $# -eq 0 ]; then
-    echo "Usage: $0 LEVEL AUTHOR"
+    echo "Usage: $0 LEVEL AUTHOR [hs]"
     exit
+fi
+
+hs_prefix=""
+if [ $# -ge 3 -a "$3" == "hs" ]; then
+    hs_prefix="_hs"
 fi
 
 mkdir -p $EXTRACT_FOLDER
 
-tar xzf volume/n_levels/"$1".ltm -C extract/
+tar xzf volume/n_levels/"$1"${hs_prefix}.ltm -C extract/
 sed -i "s/authors=.*/authors=$2/" extract/config.ini
-tar czf $DOCKER_VOLUME_PATH/n_levels/"$1".ltm -C $EXTRACT_FOLDER . --transform='s|^\./||'
+tar czf $DOCKER_VOLUME_PATH/n_levels/"$1"${hs_prefix}.ltm -C $EXTRACT_FOLDER . --transform='s|^\./||'
