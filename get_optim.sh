ebf65f475384489c6087e955cdb66b19c920a354
Célestin Matte <dev@cmatte.me>
2026-09-17 17:47:12 +0200

    Use generic path for optimiser output

--- get_optim.sh
+++ get_optim.sh
@@ -7,8 +7,7 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"
 
 EXTRACT_FOLDER="extract"
-#OPTIM_FILE="tas optimiser/wip/optim.ltm"
-OPTIM_FILE="volume/n_levels/57-3_optimised.ltm"
+OPTIM_FILE="tas optimiser/wip/optim.ltm"
 
 tar xzf "$OPTIM_FILE" -C $EXTRACT_FOLDER
 tail -n +41 $EXTRACT_FOLDER/inputs | xclip -selection clipboard
