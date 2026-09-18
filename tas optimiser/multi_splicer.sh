3f306c396e8fea84a28828b5d1b38e85b4db11d3
Célestin Matte <dev@cmatte.me>
2026-09-18 13:00:58 +0200

    multi_splicer: ensure level number is always in temporary ltm file name

--- tas optimiser/multi_splicer.sh
+++ tas optimiser/multi_splicer.sh
@@ -22,6 +22,6 @@ for i in levels/$1/*.txt ; do
     splice_ext=$(basename $i)
     splice=${splice_ext%.txt}
     echo -e "\033[32m************** Running ${splice_ext}\033[0m"
-    python3 optimize_replay.py local "$ORIG" --config "$i" --output wip/$splice.ltm --replay-output wip/$splice.txt --stagnation-rounds 1 --workers 15
-    ORIG=wip/$splice.ltm
+    python3 optimize_replay.py local "$ORIG" --config "$i" --output wip/$1_$splice.ltm --replay-output wip/$1_$splice.txt --stagnation-rounds 1 --workers 15
+    ORIG=wip/$1_$splice.ltm
 done
