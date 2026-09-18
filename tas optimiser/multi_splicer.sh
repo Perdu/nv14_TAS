338113811983201a4b95edd05683c0e071ed0c05
Célestin Matte <dev@cmatte.me>
2026-09-18 13:18:25 +0200

    multi_splicer: ensure files are executed in order, even with >1000f files

--- tas optimiser/multi_splicer.sh
+++ tas optimiser/multi_splicer.sh
@@ -18,12 +18,12 @@ if [ ! -d levels/$1 ] ; then
 fi
 
 ORIG="../n_levels/$1.ltm"
-for i in levels/$1/*.txt ; do
+while IFS= read -r i; do
     splice_ext=$(basename $i)
     splice=${splice_ext%.txt}
     echo -e "\033[32m************** Running ${splice_ext}\033[0m"
-    python3 optimize_replay.py local "$ORIG" --config "$i" --output wip/$1_$splice.ltm --replay-output wip/$1_$splice.txt --stagnation-rounds 1 --workers 15
+    #python3 optimize_replay.py local "$ORIG" --config "$i" --output wip/$1_$splice.ltm --replay-output wip/$1_$splice.txt --stagnation-rounds 1 --workers 15
     ORIG=wip/$1_$splice.ltm
-done
+done < <(printf '%s\n' levels/$1/*.txt | sort -V)
 
 cp $ORIG wip/optim.ltm
