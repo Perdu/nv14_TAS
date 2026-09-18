1c0a6c7f496509bc5d1a255add13a5c43a89c81a
Célestin Matte <dev@cmatte.me>
2026-09-18 16:00:57 +0200

    n_start: enable keeping region markers by default

--- volume/lua/n_start.lua
+++ volume/lua/n_start.lua
@@ -28,7 +28,7 @@ splice_files_path = "/home/splices/"
 splice_region_size = 20
 splice_prev_range_prior_frames = 10
 display_switches_numbers = true
-clean_splices_region_markers = true
+clean_splices_region_markers = false
 
 ---- Constants
 SAVE_SLOT = 1             -- Save slot number (1–10)
