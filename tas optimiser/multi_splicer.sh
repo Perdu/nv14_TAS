e61156e6a9a7b4f60aeae331ab625c3380e66637
Célestin Matte <dev@cmatte.me>
2026-09-25 12:42:28 +0200

    multi_splicer: add option to restart from a certain frame number

--- tas optimiser/multi_splicer.sh
+++ tas optimiser/multi_splicer.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"
 
 function usage() {
-    echo "Usage: $0 LEVEL"
+    echo "Usage: $0 LEVEL START"
     exit $1
 }
 
@@ -24,6 +24,15 @@ ORIG="../n_levels/$1.ltm"
 while IFS= read -r i; do
     splice_ext=$(basename $i)
     splice=${splice_ext%.txt}
+
+    # If $2 is provided, skip files with a number <= $2
+    if [[ -n ${2:-} && $splice_ext =~ ^([0-9]+)(_|[.]) ]]; then
+        num=${BASH_REMATCH[1]}
+        if (( 10#$num <= 10#$2 )); then
+            continue
+        fi
+    fi
+
     echo -e "\033[32m************** Running ${splice_ext}\033[0m"
     python3 optimize_replay.py local "$ORIG" --config "$i" --output wip/$1_$splice.ltm --replay-output wip/$1_$splice.txt --stagnation-rounds 1 --workers 15
     ORIG=wip/$1_$splice.ltm
